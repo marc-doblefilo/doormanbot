c3149bb18f0bffca61cbc733480c41096663690d
Proph3t <rodriguezmoreno.marcos@gmail.com>
2020-07-30 03:40:26 +0200

    Added function

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import os
 from decouple import config
 import telebot
+from telebot import types
 
 TELEGRAM_TOKEN = config('API')
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
 @bot.message_handler(commands=['info'])
-def send_message(message):
-    bot.reply_to(message, "HELLO")
-
+def verify_user(message):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    Verify_Button = types.KeyboardButton("I'm not a robot.")
+    markup.add(Verify_Button)
+    bot.send_message(message.chat.id, "Verify you're not a robot", reply_markup=markup)
 
 bot.polling()
